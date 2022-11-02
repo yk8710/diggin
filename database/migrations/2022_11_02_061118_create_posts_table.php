67198df78318403a03e182ed1220183f56686916
yk8710 <y.kaneko198710@gmail.com>
2022-11-02 15:47:57 +0900

    マイグレーション実行

---
+++ database/migrations/2022_11_02_061118_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('artist_name');
+            $table->string('track_name');
+            $table->string('album_name');
+            $table->string('label');
+            $table->integer('year');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
