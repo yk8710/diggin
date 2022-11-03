defdbe5eefb294b415f3ca01cfc3c21f14db68e3
yk8710 <y.kaneko198710@gmail.com>
2022-11-03 16:46:56 +0900

    入力値の保持

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,15 +1,15 @@
 @csrf
 <dl>
     <dt>アーティスト名</dt>
-    <dd><input type="text" name="artist_name"></dd>
+    <dd><input type="text" name="artist_name" value="{{ old('artist_name') }}"></dd>
     <dt>曲名</dt>
-    <dd><input type="text" name="track_name"></dd>
+    <dd><input type="text" name="track_name" value="{{ old('track_name') }}"></dd>
     <dt>アルバム名</dt>
-    <dd><input type="text" name="album_name"></dd>
+    <dd><input type="text" name="album_name" value="{{old('album_name')}}"></dd>
     <dt>レーベル名</dt>
-    <dd><input type="text" name="label"></dd>
+    <dd><input type="text" name="label" value="{{ old('label') }}"></dd>
     <dt>リリース年</dt>
-    <dd><input type="text" name="year"></dd>
+    <dd><input type="text" name="year" value="{{ old('year') }}"></dd>
     <dt>コンテンツ</dt>
-    <dd><textarea name="content" cols="30" rows="10"></textarea></dd>
+    <dd><textarea name="content" cols="30" rows="10">{{ old('content') }}</textarea></dd>
 </dl>
