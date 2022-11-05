f521e517e66591fa193059c3c55c17dd61b263a7
yk8710 <y.kaneko198710@gmail.com>
2022-11-05 10:01:18 +0900

    ルーティングまとめ

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostsController::class, 'index'])
-    ->name('posts.index');
-Route::get('/posts/create', [PostsController::class, 'create'])
-    ->name('posts.create');
-Route::post('/posts', [PostsController::class, 'store'])
-    ->name('posts.store');
-Route::get('/posts/{post}', [PostsController::class, 'show'])
-    ->name('posts.show');
-Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])
-    ->name('posts.edit');
-Route::patch('posts/{post}', [PostsController::class, 'update'])
-    ->name('posts.update');
-Route::delete('/posts/{post}', [PostsController::class, 'destroy'])
-    ->name('posts.destroy');
+Route::resource('/posts', PostsController::class);
 
