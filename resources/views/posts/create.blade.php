a1927b0c92dbf76556ec157dae30f8254adfc6c5
yk8710 <y.kaneko198710@gmail.com>
2022-11-14 18:56:38 +0900

    レイアウト修正

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,13 @@
 @include('commons.errors')
 <form action="{{ route('posts.store') }}" method="post">
     @include('posts.form')
-    <button type="submit">投稿する</button>
-    <a href="{{ route('posts.index') }}">キャンセル</a>
+    <div class="text-center">
+        <div>
+            <button type="submit" class="w-full max-w-[550px] bg-blue-500 px-4 py-3 rounded text-gray-200 font-semibold hover:bg-blue-600 transition duration-200 each-in-out">投稿</button>
+        </div>
+    </div>
+    <div class="text-center mt-10">
+        <button type="button" class="w-full max-w-[550px] bg-blue-500 px-4 py-3 rounded text-gray-200 font-semibold hover:bg-blue-600 transition duration-200 each-in-out"><a href="{{ route('posts.index') }}">キャンセル</a></button>
+    </div>
 </form>
 @endsection
--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -1,15 +1,93 @@
 @csrf
-<dl>
-    <dt>アーティスト名</dt>
-    <dd><input type="text" name="artist_name" value="{{ old('artist_name', $post->artist_name) }}"></dd>
-    <dt>曲名</dt>
-    <dd><input type="text" name="track_name" value="{{ old('track_name', $post->track_name) }}"></dd>
-    <dt>アルバム名</dt>
-    <dd><input type="text" name="album_name" value="{{ old('album_name', $post->album_name) }}"></dd>
-    <dt>レーベル名</dt>
-    <dd><input type="text" name="label" value="{{ old('label', $post->label) }}"></dd>
-    <dt>リリース年</dt>
-    <dd><input type="text" name="year" value="{{ old('year', $post->year) }}"></dd>
-    <dt>コンテンツ</dt>
-    <dd><textarea name="content" cols="30" rows="10">{{ old('content', $post->content) }}</textarea></dd>
-</dl>
+<!-- component -->
+<div class="flex items-center justify-center p-12">
+    <!-- Author: FormBold Team -->
+    <!-- Learn More: https://formbold.com -->
+    <div class="mx-auto w-full max-w-[550px]">
+        <div class="mb-5">
+          <label
+            for="artist_name"
+            class="mb-3 block text-base font-medium text-[#07074D]"
+          >
+            アーティスト名
+          </label>
+          <input
+            type="text"
+            name="artist_name"
+            value="{{ old('artist_name', $post->artist_name) }}"
+            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+          />
+        </div>
+        <div class="mb-5">
+          <label
+            for="track_name"
+            class="mb-3 block text-base font-medium text-[#07074D]"
+          >
+            曲名
+          </label>
+          <input
+            type="text"
+            name="track_name"
+            value="{{ old('track_name', $post->track_name) }}"
+            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+          />
+        </div>
+        <div class="mb-5">
+            <label
+              for="track_name"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              アルバム名
+            </label>
+            <input
+              type="text"
+              name="album_name"
+              value="{{ old('album_name', $post->album_name) }}"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+        <div class="mb-5">
+          <label
+            for="label"
+            class="mb-3 block text-base font-medium text-[#07074D]"
+          >
+            レーベル名
+          </label>
+          <input
+            type="text"
+            name="label"
+            value="{{ old('label', $post->label) }}"
+            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+          />
+        </div>
+        <div class="mb-5">
+            <label
+              for="year"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              リリース年
+            </label>
+            <input
+              type="text"
+              name="year"
+              value="{{ old('year', $post->year) }}"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+        <div class="mb-5">
+          <label
+            for="content"
+            class="mb-3 block text-base font-medium text-[#07074D]"
+          >
+            コンテンツ
+          </label>
+          <textarea
+            cols="30"
+            rows="10"
+            name="content"
+            class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+          >{{ old('content', $post->content) }}</textarea>
+        </div>
+    </div>
+  </div>
+
